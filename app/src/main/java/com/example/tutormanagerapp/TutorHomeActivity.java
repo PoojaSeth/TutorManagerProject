7efd90adb5c7f4a013320217b4c1ba81ae2657aa
Scott Schoeller <23561648+sschoellerSTEM@users.noreply.github.com>
2019-03-30 12:52:57 -0500

    Changes to Tutor files
    Organization, Comments

--- app/src/main/java/com/example/tutormanagerapp/TutorHomeActivity.java
+++ app/src/main/java/com/example/tutormanagerapp/TutorHomeActivity.java
@@ -1,70 +1,110 @@
 package com.example.tutormanagerapp;
 
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+// Import UI elements
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+// Import Firebase
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+
 public class TutorHomeActivity extends AppCompatActivity {
 
+    // Firebase
     private FirebaseUser user;
     private DatabaseReference reference;
 
+    // Navigation
+    private Fragment selectedFragment;
+
+    // Profile
     CircleImageView profilepic;
     TextView nameTV;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_home);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
+        nameTV = (TextView) findViewById(R.id.nameTV);
 
-        nameTV = (TextView)findViewById(R.id.nameTV);
-        profilepic = (CircleImageView)findViewById(R.id.profilepic);
-
+        profilepic = (CircleImageView) findViewById(R.id.profilepic);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Tutors").child(user.getUid());
+        reference = FirebaseDatabase.getInstance().getReference("Students").child(user.getUid());
+
+        //reference.addValueEventListener(new ValueEventListener());
+
+        name = user.getDisplayName(); // getting the name from database
+
+        nameTV.setText(name); // displaying it in the textview
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().
                 replace(R.id.fragment_container, new TutorHomeFragment()).commit();
+
     }
+
+        //@Override
+        public void onDataChange (@NonNull DataSnapshot dataSnapshot) {
+        Student student = dataSnapshot.getValue(Student.class);
+        assert student != null;
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(" ");
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //navListener = OnMenuItemClickListener();
+        //bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+            getSupportFragmentManager().beginTransaction().
+                    replace(R.id.fragment_container, new TutorHomeFragment()).commit();
+
+    }
+
+
+    //@Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFragment = null;
-            switch (menuItem.getItemId()){
-                case R.id.nav_home:
-                    selectedFragment = new TutorHomeFragment();
-                    break;
+
+            switch (menuItem.getItemId()) {
+                //case R.id.nav_home:
+                //selectedFragment = new TutorHomeFragment();
+                //break;
                 case R.id.nav_profile:
                     selectedFragment = new TutorProfileFragment();
                     break;
                 case R.id.nav_chat:
                     selectedFragment = new TutorChatFragment();
                     break;
-
+                default: // Tutor's home
+                    selectedFragment = new TutorHomeFragment();
             }
+
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
-                    ,selectedFragment).commit();
+                    , selectedFragment).commit();
 
             return true;
         }
--- app/src/main/java/com/example/tutormanagerapp/TutorProfileFragment.java
+++ app/src/main/java/com/example/tutormanagerapp/TutorProfileFragment.java
@@ -3,6 +3,8 @@ package com.example.tutormanagerapp;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,5 +12,12 @@ import android.view.ViewGroup;
 
 
 public class TutorProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tutor_profile,container,false);
+
+    }
 
 }
